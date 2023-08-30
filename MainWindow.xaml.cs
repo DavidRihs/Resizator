87a096d36051bc369c12832a26006a7dfdfb74a6
David Rihs <davidrihs@gmail.com>
2023-08-31 00:31:47 +0200

    Unninstall appdata directory Embeded PBD

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -81,6 +81,7 @@ namespace Resizator
                 process.Close();
 
                 Compress();
+                WriteToConsole("Done !");
             }
             catch (Exception ex)
             {
